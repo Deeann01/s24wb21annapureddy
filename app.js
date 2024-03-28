841b4f3e2977750fac5aee67f9213a1d7c47ca34
Deeann01 <deepika.annapureddy04@gmail.com>
2024-03-27 22:35:27 -0500

    added

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var Aquariumclass=require('./routes/Aquariums');
+var gridRouter=require('./routes/grid');
+var pickRouter=require('./routes/pick');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/Aquariums',Aquariumclass);
+app.use('/grid',gridRouter);
+app.use('/pick',pickRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +45,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
